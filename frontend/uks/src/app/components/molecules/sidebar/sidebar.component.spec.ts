c50ffd92c25c4109d54b395d797a2c8066e3c2a3
Dragan Mirkovic <63748840+Dragan2402@users.noreply.github.com>
2024-01-11 20:17:18 +0100

    Feature 41: Created CICD for angular app (#42)
    * Feature 41: Created CICD for angular app
    
    * Feature-41: Changed first workflow
    
    * Feature 41: Changed node version
    
    * Feature 41: Changed npm test script
    
    * Feature 41: Changed npm test parameters
    
    * Feature 41: Changed test

--- frontend/uks/src/app/components/molecules/sidebar/sidebar.component.spec.ts
+++ frontend/uks/src/app/components/molecules/sidebar/sidebar.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { SidebarComponent } from './sidebar.component';
 
@@ -8,9 +9,9 @@ describe('SidebarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
-    })
-    .compileComponents();
+      declarations: [SidebarComponent],
+      imports: [HttpClientModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
