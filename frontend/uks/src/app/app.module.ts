a2fbe8bdd4e90c2d5fa5201d2dd75ab1f96b44ae
Dragan Mirkovic <draganmirkovic2402@gmail.com>
2024-01-17 17:37:27 +0100

    Update app.module.ts

--- frontend/uks/src/app/app.module.ts
+++ frontend/uks/src/app/app.module.ts
@@ -1,32 +1,33 @@
+import { ToastrModule } from 'ngx-toastr';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './components/molecules/navbar/navbar.component';
-import { PageTemplateComponent } from './components/organisms/page-template/page-template.component';
-import { LoginPageComponent } from './pages/authentication/login-page/login-page.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { RepositoryPageComponent } from './pages/repository-page/repository-page.component';
-import { SidebarComponent } from './components/molecules/sidebar/sidebar.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { TabsComponent } from './components/molecules/tabs/tabs.component';
-import { ProjectIssuesComponent } from './components/organisms/project-issues/project-issues.component';
-import { ButtonComponent } from './components/molecules/button/button.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { InputComponent } from './components/molecules/input/input.component';
-import { SignUpPageComponent } from './pages/authentication/sign-up-page/sign-up-page.component';
-import { ForgotPasswordPageComponent } from './pages/authentication/forgot-password-page/forgot-password-page.component';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatSelectModule } from '@angular/material/select';
-import { ToastrModule } from 'ngx-toastr';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ButtonComponent } from './components/molecules/button/button.component';
+import { InputComponent } from './components/molecules/input/input.component';
+import { NavbarComponent } from './components/molecules/navbar/navbar.component';
+import { SidebarComponent } from './components/molecules/sidebar/sidebar.component';
+import { TabsComponent } from './components/molecules/tabs/tabs.component';
+import { PageTemplateComponent } from './components/organisms/page-template/page-template.component';
+import { ProjectIssuesComponent } from './components/organisms/project-issues/project-issues.component';
+import { ForgotPasswordPageComponent } from './pages/authentication/forgot-password-page/forgot-password-page.component';
+import { LoginPageComponent } from './pages/authentication/login-page/login-page.component';
+import { SignUpPageComponent } from './pages/authentication/sign-up-page/sign-up-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { RepositoryPageComponent } from './pages/repository-page/repository-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -60,9 +61,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
