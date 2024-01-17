5a43065334acb7f87c34268aa13a8e4a075f9468
Dragan Mirkovic <draganmirkovic2402@gmail.com>
2024-01-17 18:28:39 +0100

    Fixed merge issues

--- frontend/uks/src/app/components/molecules/sidebar/sidebar.component.ts
+++ frontend/uks/src/app/components/molecules/sidebar/sidebar.component.ts
@@ -1,37 +1,33 @@
+import { SelectionOptions } from 'src/models/navigation';
+import { UserBasicInfo } from 'src/models/user/user';
+import { AuthService } from 'src/services/auth/auth.service';
+
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
-import { SelectionOptions } from 'src/models/navigation';
-import { LoggedUser } from 'src/models/user/user';
-import { AuthService } from 'src/services/auth/auth.service';
-
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
-
   @ViewChild('drawer') drawer: MatDrawer | undefined;
   SelectionOptions = SelectionOptions;
 
-  loggedUser: LoggedUser | undefined;
+  loggedUser: UserBasicInfo | undefined;
 
-  constructor(
-    private router: Router,
-    private authService: AuthService
-  ) { }
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {
     this.authService.getLoggedUser().subscribe({
-      next: (user: LoggedUser | undefined) => {
+      next: (user: UserBasicInfo | undefined) => {
         this.loggedUser = user;
       },
       error: (e: any) => {
         console.log(e.error);
-      }
-    })
+      },
+    });
   }
 
   navigate(option: SelectionOptions): void {
