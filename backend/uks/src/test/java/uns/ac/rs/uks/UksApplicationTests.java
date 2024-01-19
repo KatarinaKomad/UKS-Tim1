9e08b0e5d5e4987d79ff3339268c7625a352743c
banzg00 <bane-gg@hotmail.com>
2024-01-19 13:47:18 +0100

    Update UksApplicationTests.java

--- backend/uks/src/test/java/uns/ac/rs/uks/UksApplicationTests.java
+++ backend/uks/src/test/java/uns/ac/rs/uks/UksApplicationTests.java
@@ -3,12 +3,14 @@ package uns.ac.rs.uks;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
 @SpringBootTest
+@EnableCaching
 class UksApplicationTests {
 
 	@Test
