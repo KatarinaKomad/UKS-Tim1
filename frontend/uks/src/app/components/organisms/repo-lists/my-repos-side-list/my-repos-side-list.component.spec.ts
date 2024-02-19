3847c33896c23bbb72092049f34c380c12d1e9be
Nikola Jovic <jovnikola99@gmail.com>
2024-02-20 00:04:34 +0100

    Test fixed pt3

--- frontend/uks/src/app/components/organisms/repo-lists/my-repos-side-list/my-repos-side-list.component.spec.ts
+++ frontend/uks/src/app/components/organisms/repo-lists/my-repos-side-list/my-repos-side-list.component.spec.ts
@@ -1,21 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 
-import { RepoSettingsComponent } from './repo-settings.component';
+import { MyReposSideListComponent } from './my-repos-side-list.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 describe('MyReposSideListComponent', () => {
-  let component: RepoSettingsComponent;
-  let fixture: ComponentFixture<RepoSettingsComponent>;
+  let component: MyReposSideListComponent;
+  let fixture: ComponentFixture<MyReposSideListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-      declarations: [RepoSettingsComponent]
+      imports: [MatDialogModule, HttpClientModule],
+      declarations: [MyReposSideListComponent]
     })
       .compileComponents();
 
 
-    fixture = TestBed.createComponent(RepoSettingsComponent);
+    fixture = TestBed.createComponent(MyReposSideListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
