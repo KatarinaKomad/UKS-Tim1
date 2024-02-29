8bb4dc5d93e6ea7e8a3bbc654ecaa1a54b749c39
KatarinaKomad <komad.katarina@gmail.com>
2024-02-29 13:41:12 +0100

    Fixed failed test

--- frontend/uks/src/app/components/molecules/dialogs/yes-no-dialog/yes-no-dialog.component.spec.ts
+++ frontend/uks/src/app/components/molecules/dialogs/yes-no-dialog/yes-no-dialog.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { YesNoDialogComponent } from './yes-no-dialog.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 describe('YesNoDialogComponent', () => {
   let component: YesNoDialogComponent;
@@ -8,10 +10,16 @@ describe('YesNoDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [YesNoDialogComponent]
+      imports: [HttpClientModule, MatDialogModule],
+      declarations: [YesNoDialogComponent],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} }
+      ]
+
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(YesNoDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
