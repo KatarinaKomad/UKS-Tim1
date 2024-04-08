db56b2e8f27ffb13a83a1eaa64271ab63d97cf9d
Nikola Jovic <jovnikola99@gmail.com>
2024-04-08 16:21:34 +0200

    Test solved

--- frontend/uks/src/services/comment/comment.service.spec.ts
+++ frontend/uks/src/services/comment/comment.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 import { CommentService } from './comment.service';
 
@@ -6,7 +7,10 @@ describe('CommentService', () => {
   let service: CommentService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [CommentService],
+    });
     service = TestBed.inject(CommentService);
   });
 
